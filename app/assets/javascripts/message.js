bce8eed2769c5fdfe38d6b658beb380c60154a64
kanzakidaisuke <daisuke044880@gmail.com>
2020-09-26 21:33:29 +0900

    commit

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="messageBox">
+          <div class="messageInfo">
+            <div class="messageInfo__userName">
+              ${message.user_name}
+            </div>
+            <div class="messageInfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+            <img class="message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="messageBox">
+        <div class="messageInfo">
+          <div class="messageInfo__userName">
+            ${message.user_name}
+          </div>
+          <div class="messageInfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.Form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.message-list').append(html);      
+      $('form')[0].reset();
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      $(".Form__submit").attr('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  });
+});
